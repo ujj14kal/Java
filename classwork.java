80f2e55fa517e8cbbe6d82b266a5abe108839985
ujj14kal <ujj.kalra10@gmail.com>
2025-08-22 13:43:56 +0530

    DAY 1

---
+++ classwork.java
@@ -0,0 +1,58 @@
+//  JAVA CODE FOR PRINTING ALL IN-LINE STRINGS ENTERED
+
+class students {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
+
+// STATIC, LOCAL AND INSTANCE VARIABLES
+
+class stata {
+    static int x;
+    static int count;
+
+    public void update() {
+        x++;
+        count++;
+    }
+
+    public void show() {
+        System.out.println("count=" + count);
+        System.out.println("x=" + x);
+    }
+
+    // Static Local and instance variables
+    public static void main(String arg[]) {
+        stata s1 = new stata(); // Object creation
+        s1.update();
+        stata s2 = new stata();
+        s2.update();
+        stata s3 = new stata();
+        s3.update();
+        s1.show();
+
+    }
+
+}
+
+// DIFFERENCE IN COMPULE TIME OVERLOADING AND METHOD OVERLOADING
+
+class Main {
+    void show(int a, double num2) {
+        System.out.println("Integer: " + num2);
+    }
+
+    void show(double a, int num2) {
+        System.out.println("Two integer values:" + a + "and" + num2);
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.show(10d, 20);
+
+    }
+}
